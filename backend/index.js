f7a17b23ca01fc8dcad13554ef4e456c1b7c4870
Your Name <your_email@example.com>
2025-01-30 19:27:30 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,22 @@ const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import bodyParser from "body-parser";
+import cors from "cors"
+
 dotenv.config();
+//middlware
+app.use(express.json());
+app.use(bodyParser.json({limit: "50mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"50mb", extended: true}));
+app.use(cors());
+
+
 
 const PORT = process.env.PORT || 8000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
-//middlware
-app.use(express.json());
+
 
 try {
     mongoose.connect(MONGODB_URL, {
