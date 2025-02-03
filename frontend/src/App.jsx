83175982b65264a1bd28872e48b7235d40d92b0e
Your Name <your_email@example.com>
2025-02-04 01:15:08 +0530

    routers updated in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Registration from './pages/Authentication/Registration'
+import Login from "./pages/Authentication/Login"
+import About from './pages/About'
+import Contact from './pages/Contact'
+import AdminBlog from './pages/Dashboard/AdminBlog'
+import CreateBlog from './pages/Dashboard/CreateBlog'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Registration from './pages/registration'
-import Blog from './pages/blog'
 
 
 function App() {
 
+  const location = useLocation()
+  const hideNavbarAndFooter = ["/login", "/registration"].includes(location.pathname); 
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Registration />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </Router>
-    </>
+    <div>
+      {!hideNavbarAndFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/adminblog" element={<AdminBlog />} />
+        <Route path="/createblog" element={<CreateBlog />} />
+
+      </Routes>
+      {!hideNavbarAndFooter && <Footer />}
+    </div>
   )
 }
 
